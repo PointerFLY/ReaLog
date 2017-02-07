f7c67fb886b42f50573fbf217aab55ee7eb47949
PointerFLY <linghao.michael.ma@gmail.com>
2017-02-07 15:41:34 +0800

    Initialize project

---
+++ ReaLog/ReaLog.h
@@ -0,0 +1,19 @@
+//
+//  ReaLog.h
+//  ReaLog
+//
+//  Created by PointerFLY on 07/02/2017.
+//  Copyright © 2017 PointerFLY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReaLog.
+FOUNDATION_EXPORT double ReaLogVersionNumber;
+
+//! Project version string for ReaLog.
+FOUNDATION_EXPORT const unsigned char ReaLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReaLog/PublicHeader.h>
+
+
